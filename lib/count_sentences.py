c5c726efbf3b4e4ce89f74518b71fd40a62a99d6
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-12-13 08:50:03 -0800

    Lab Complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
+import re
+import sys
+import io
+
+
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = ""
+        self.value = value
+        
+    @property
+    def value(self):
+      return self._value
+    
+    @value.setter
+    def value(self, value):
+      if isinstance(value, str):
+        self._value = value
+      else:
+        print("The value must be a string.")
+        self.value = ""
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        sentences = re.split(r"[.!?]+", self.value)
+        sentences = [s.strip() for s in sentences if s.strip()]
+
+        return len(sentences)
